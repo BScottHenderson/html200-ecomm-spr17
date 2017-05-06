d776d88e30d6e71453deb4d7d9a504a41ec41abe
Scott Henderson <henders999@gmail.com>
2017-05-05 21:07:59 -0700

    Added error handling.

--- js/mailing-list.js
+++ js/mailing-list.js
@@ -1,7 +1,11 @@
 
 function welcomeToMailingList() {
-  console.log("Welcome to our mailing list " + document.mailingList.email.value + "!");
-  console.log("Now with 80% less content!")
+  if (document.mailingList.email.value) {
+    console.log("Welcome to our mailing list " + document.mailingList.email.value + "!");
+    console.log("Now with 80% less content!")
+  } else {
+    console.log("Please enter an e-mail address.");
+  }
 
   event.preventDefault(); // Prevent the form from submitting to the server.
                           // If we don't do this then the stuff we wrote to the console
