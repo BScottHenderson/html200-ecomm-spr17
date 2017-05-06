269463ac6344a36cedac401af5ccc3f7eef74829
Scott Henderson <henders999@gmail.com>
2017-05-05 20:58:11 -0700

    Removed overstrike from console message.  It is possible to write formatted html using console.log (at least in Chrome) but only on a new line.

--- js/mailing-list.js
+++ js/mailing-list.js
@@ -1,7 +1,6 @@
 
 function welcomeToMailingList() {
-  console.log("Welcome to our <overstrike>junk</overstrike> mailing list " +
-              document.mailing-list.email.value + "!");
+  console.log("Welcome to our mailing list " + document.mailingList.email.value + "!");
   console.log("Now with 80% less content!")
 
   event.preventDefault(); // Prevent the form from submitting to the server.
