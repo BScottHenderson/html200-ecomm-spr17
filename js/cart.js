cd85721f275676f6fa73f2b357f0ccde0ce23c8b
Scott Henderson <henders999@gmail.com>
2017-05-11 16:02:10 -0700

    Added shopping cart array and methods.

---
+++ js/cart.js
@@ -0,0 +1,35 @@
+
+var cart = [];
+
+function findWithAttr(array, attr, value) {
+    for (var i = 0; i < array.length; ++i) {
+        if (array[i][attr] === value) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function addToCart(_productID) {
+
+  if (findWithAttr(cart, "productID", _productID) == -1) {
+    var i = findWithAttr(products, "productID", _productID);
+    cart.push(products[i]);
+  }
+
+  console.log("Cart length = " + cart.length);
+
+  event.preventDefault();
+}
+
+function removeFromCart(_productID) {
+
+  var i = findWithAttr(cart, "productID", _productID);
+  if (i != -1) {
+    cart.splice(i, 1);
+  }
+
+  console.log("Cart length = " + cart.length);
+
+  event.preventDefault();
+}
